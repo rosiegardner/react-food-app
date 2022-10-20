ac392d2bef6ec83823b4d3b43cec8ff4e38520b7
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-20 14:34:59 -0700

    update: accept an id prop on mealitemform component to create a unique id per input

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -8,13 +8,14 @@ const MealItemForm = (props) => {
       <Input 
         label="Amount"
         input={{
-          id: 'amount',
+          id: 'amount_' + props.id,
           type: 'number',
           min: '1',
           max: '5',
           step: '1',
           defaultValue: '1'
-        }} />
+        }} 
+      />
       <button>+ Add</button>
     </form>
   );
