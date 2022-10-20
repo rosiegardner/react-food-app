232b26d45b010e45485a9f2c9af0e3e18d6e95f8
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-20 14:09:12 -0700

    import Input components with values

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import Input from '../../UI/Input';
 import classes from '../.././../styles/Meals/MealItemForm.module.css';
 
 const MealItemForm = (props) => {
   return (
     <form className={classes.form}>
-      <input />
+      <Input 
+        label="Amount"
+        input={{
+          id: 'amount',
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1'
+        }} />
       <button>+ Add</button>
     </form>
   );
