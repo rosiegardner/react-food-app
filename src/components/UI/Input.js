85adf4d4948bbb0844465227658d316f41a3c675
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-20 14:07:37 -0700

    create: Input component for items

---
+++ src/components/UI/Input.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import classes from '../.././styles/UI/Input.module.css';
+
+const Input = (props) => {
+  return(
+    <div className={classes.input}>
+      <label htmlFor={props.input.id}>
+        {props.label}
+      </label>
+      <input  
+        {...props.input} 
+      />
+    </div>
+  );
+}
+
+export default Input;
