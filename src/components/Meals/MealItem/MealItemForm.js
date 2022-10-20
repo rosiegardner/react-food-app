a8a698a6ca21aa374e34ae44b0b16486b330cf17
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-20 13:11:08 -0700

    create: MealItemForm component

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from '../.././../styles/Meals/MealItemForm.module.css';
+
+const MealItemForm = (props) => {
+  return (
+    <form className={classes.form}>
+      <input />
+      <button>+ Add</button>
+    </form>
+  );
+}
+
+export default MealItemForm;
