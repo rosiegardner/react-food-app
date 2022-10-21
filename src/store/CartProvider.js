9ae00143ceee37c162b702abe640aed1bf2475d5
Rosie Gardner <rosiegardner78@gmail.com>
2022-10-20 18:33:17 -0700

    remove: whitespace from spread operator

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -46,7 +46,7 @@ const cartReducer = (state, action) => {
     if (existingItem.amount ===1 ) {
       updatedItems = state.items.filter(item => item.id !== action.id);
     } else {
-      const updatedItem = { ... existingItem, 
+      const updatedItem = { ...existingItem, 
         amount: existingItem.amount - 1 
       };
       updatedItems = [ ...state.items ];
